c8607802d8a542493ade6d0d2d4599f9639353e5
Icmp <71145797+IcmpOff@users.noreply.github.com>
2021-08-03 09:58:32 -0700

    SourceCode Upload

---
+++ Auth/Authentication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Mailify
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Utilities.GetColors();
+            Utilities.GetColor();
+            Console.Title = $"Mailify [{Variables.version}] - Made by YoBoi";
+            Menu.mainmenu();
+        }
+    }
+}
+
+
